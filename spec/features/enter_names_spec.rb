7686c99df03cdbc625a36f0cddf5793fa852120b
yurizhuravel <zhuravel@gmail.com>
2016-07-19 17:17:46 +0100

    Adds helper file for refactoring

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require './app.rb'
 
 feature 'Player names', :type => :feature do
   scenario "Players enter their names" do
-    visit ('/')
-    fill_in "player_1", :with => "Dan"
-    fill_in "player_2", :with => "Yuri"
-    click_button "Submit names"
+    sign_in_and_play
     expect(page).to have_text("Dan vs. Yuri")
   end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -2,10 +2,7 @@ require './app.rb'
 
 feature 'Hit points', :type => :feature do
   scenario "Players see each other's hit points" do
-    visit ('/')
-    fill_in :player_1, :with => "Dan"
-    fill_in :player_2, :with => "Yuri"
-    click_button "Submit names"
+    sign_in_and_play
     expect(page).to have_text("Yuri: 60HP")
   end
 end
