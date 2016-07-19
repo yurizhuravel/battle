224d79bcbe412bb01e6c709170c1dfbdcccd0cb1
dan-cl <djclissold@yahoo.co.uk>
2016-07-19 16:07:49 +0100

    18. Entering Players

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
+require 'shotgun'
 
 class Battle < Sinatra::Base
   get '/' do
     'Hello Battle!'
+    erb(:index)
   end
 
+  post '/names' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:play)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,11 @@
+require '.././app.rb'
+
+RSpec.feature 'Player names', :type => :feature do
+  scenario "Players enter their names" do
+    visit ('/')
+    fill_in "player_1", :with => "Dan"
+    fill_in "player_2", :with => "Yuri"
+    click_button "Submit names"
+    expect(page).to have_text("Dan vs. Yuri")
+  end
+end
