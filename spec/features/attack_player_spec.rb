8d4bc17ff71e340f6bacf1c48863da56b6e9feb8
yurizhuravel <zhuravel@gmail.com>
2016-07-20 10:29:24 +0100

    Adds attack functionality

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -3,7 +3,7 @@ require './app.rb'
 feature 'Attack player' do
   scenario "Player attacks" do
     sign_in_and_play
-    click_link "Attack!!"
-    expect(page).to have_content("Player attacked!")
+    click_button "Attack!!"
+    expect(page).to have_content("Dan attacked Yuri")
   end
 end
