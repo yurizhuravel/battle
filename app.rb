fee18568307fcb2a12604fdaaeca95c3de6ea035
Georgia Mills <georgee_123@hotmail.com>
2016-07-20 17:06:31 +0100

    skinny congtroller with game class"

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -10,21 +11,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    player_1 = Player.new(params[:player_1])
+    player_2 = Player.new(params[:player_2])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    Game.new.attack($player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,25 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new }
-  let(:chuck) { double :player }
-  let(:wayne) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
 
   describe '#attack' do
     it 'damages the player' do
-      expect(wayne).to receive(:receive_damage)
-      game.attack(wayne)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
     end
   end
 
+  describe '#player_1' do
+    it 'stores the name of player_1' do
+     expect(game.player_1).to eq player_1
+    end
+  end
+  describe '#player_2' do
+    it 'stores the name of player_2' do
+      expect(game.player_2).to eq player_2
+    end
+  end
 end
