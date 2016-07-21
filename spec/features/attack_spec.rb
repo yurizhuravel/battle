48e3eb657d1e04a396501f86c6b998b46dfd915d
Sophie Gill <sophie-gill@hotmail.com>
2016-07-21 13:56:44 +0100

    adding test for swapping players

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,12 +4,20 @@ feature 'Attack' do
   scenario "player1 attacks player2" do
     sign_in_and_play
     click_button('Attack')
-    expect(page).to have_content("Player 1 attacked player 2")
+    expect(page).to have_content("Jess attacked Luke")
   end
 
   scenario "player 2's hitpoints fall" do
     sign_in_and_play
     click_button('Attack')
-    expect(page).to have_content("Player 2 has 90hp")
+    expect(page).to have_content("Luke has 90hp")
   end
+
+  scenario 'player2 attacks player1' do
+    sign_in_and_play
+    click_button('Attack')
+    click_button('Attack')
+    expect(page).to have_content("Luke attacked Jess")
+  end
+
 end
--- spec/features/hitpoints_spec.rb
+++ spec/features/hitpoints_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 feature 'Hitpoints' do
-  scenario "player1 views player2's hitpoints" do
+  scenario "Player2's hitpoints are visible" do
     sign_in_and_play
     expect(page).to have_content("Luke 100hp")
   end
+
+  scenario "Player1's hitpoints are visible" do
+    sign_in_and_play
+    expect(page).to have_content("Jess 100hp")
+  end
+
 end
